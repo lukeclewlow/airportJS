93c02e191496b3b85364b9253bec72c43572442e
Luke Clewlow <luke.clewlow@gmail.com>
2015-01-22 09:16:40 +0000

    added helper method in testing to fill airport, first Javascript loooooop

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -23,37 +23,25 @@ describe("Airport", function() {
 			expect(airport.runway.length).toEqual(0)
 		});
 
+	});
+
 	describe("the airport", function() {
 
-			beforeEach(function() {
-			airport.landPlane(plane);
+			beforeEach(function() { 
+				for (i = 0; i < 6; i++){
+					airport.landPlane(plane);
+				}
 			});
 
 		it("should know when the runways full", function() {
-			airport.landPlane(plane)
-			airport.landPlane(plane)
-			airport.landPlane(plane)
-			airport.landPlane(plane)
-			airport.landPlane(plane)
-			airport.landPlane(plane)
-			expect(airport.isFull).toEqual(true)
+			expect(airport.isFull).toEqual(true);
 		});
 
 		it("a plane cannot land when runway is full", function() {
-			airport.landPlane(plane)
-			airport.landPlane(plane)
-			airport.landPlane(plane)
-			airport.landPlane(plane)
-			airport.landPlane(plane)
-			airport.landPlane(plane)
 			expect(airport.landPlane(plane)).toEqual("Runway full")
 		});
-		
-	});
-
 
 	});
-
 
 
 });
